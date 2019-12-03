4d178e79ed103a5f8093dbd7998990380dc34159
Daniel Liu <danliu277@gmail.com>
2019-12-03 21:29:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,8 +44,9 @@ def apply_coupons(cart, coupons)
     if item != nil && item[:count] >= coupons[coupon_index][:num]
       item[:count] -= coupons[coupon_index][:num]
       copy = {
-        :item => item[:item] += " W/COUPON",
+        :item => item[:item] + " W/COUPON",
         :count => coupons[coupon_index][:num],
+        :clearance => item[:clearance]
         :price => coupons[coupon_index][:cost] / coupons[coupon_index][:num]
       }
       cart.push(copy)
