f10ac153e169913cdfd6705c73444b43613c7e1d
Daniel Liu <danliu277@gmail.com>
2019-12-03 21:12:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,6 @@ def apply_coupons(cart, coupons)
   coupon_index = 0
   while coupons[coupon_index] do 
     item = cart.find{|x| x[:item] == coupons[coupon_index][:item]}
-    end
     # if item != nil && item[:count] > coupons[coupon_index][:num]
     #   item[:count] -= coupons[coupon_index][:count]
     #   copy = item.deep_dup
